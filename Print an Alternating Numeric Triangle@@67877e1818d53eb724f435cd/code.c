20953cdb865ca02e7f7fe9b8d52fe8e5193c3b3a
sonal2005-droid <sarkarsonal86@gmail.com>
2025-02-17 05:05:45 +0530

    Solution to Print an Alternating Numeric Triangle

--- Print an Alternating Numeric Triangle@@67877e1818d53eb724f435cd/code.c
+++ Print an Alternating Numeric Triangle@@67877e1818d53eb724f435cd/code.c
@@ -3,12 +3,12 @@
 int main() {
     int N, i, j;
 
-    printf("Enter the number of rows: ");
+    
     scanf("%d", &N);
 
     for (i = 1; i <= N; i++) {
         for (j = 1; j <= i; j++) {
-            // Print 1 if the sum of the row and column indexes is even, else print 0
+            
             printf("%d ", (i + j) % 2);
         }
         printf("\n");
